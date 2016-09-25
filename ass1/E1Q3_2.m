6dabbf446439b894b9e80476d6f81271bf16e258
redsphinx <gabi.ras@hotmail.com>
2016-09-25 18:46:41 +0200

    fix E1Q3

--- ass1/E1Q3_2.m
+++ ass1/E1Q3_2.m
@@ -2,5 +2,5 @@ M=10;
 erms_train = ERMS(train_x, train_y, M);
 erms_test = ERMS(test_x, test_y, M);
 
-plot(0:10, evec_train, 'bo-', 0:10, evec_test, 'ro-')
+plot(0:10, erms_train, 'bo-', 0:10, erms_test, 'ro-')
 legend('Training', 'Test')
