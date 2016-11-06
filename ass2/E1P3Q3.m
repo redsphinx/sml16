7656b59949cb4638b4e14c7d27e7810d3b76cf5c
redsphinx <gabi.ras@hotmail.com>
2016-11-06 13:36:41 +0100

    final edits 2

--- ass2/E1P3Q3.m
+++ ass2/E1P3Q3.m
@@ -2,19 +2,17 @@ clc
 E1P2Q1 % create mu_p and sigma_p and mu_t and sigma_t
 data = load('data.txt', '-ascii');
 n = length(data)
-% n=10
 
 mu_pr = mu_p;
 sigma_pr = sigma_p;
+% precision matrix L
 L = inv(sigma_t);
 mu_map = [];
 
 for i=1:n
-    % precision matrix L
-%     L = inv(sigma_t - sigma_pr); leave in for now
-    % compute the new posterior sigma
+    % compute the new posterior sigma -> bishop eq.2.117
     sigma_pos = inv(inv(sigma_pr) + L);
-    % compute new posterior mu
+    % compute new posterior mu -> bishop eq.2.116
     mu_pos = sigma_pos * (L * data(i,:)' + inv(sigma_pr) * mu_pr);
     
     sigma_pr = sigma_pos;
--- ass2/E1P3Q4.m
+++ ass2/E1P3Q4.m
@@ -1,24 +1,24 @@
 clf
 E1P3Q1 % get mu ML
-E1P3Q2 % get mu MAP
+E1P3Q3 % get mu MAP
 sz = 30;
 
-% figure(1)
-% % scatter( data(:,1), data(:,2), sz,  'cs',  'filled' )
-% % hold on
-% scatter(mu_ml(:,1), mu_ml(:,2), sz, 'gs')
+figure(1)
+% scatter( data(:,1), data(:,2), sz,  'cs',  'filled' )
 % hold on
-% scatter(mu_map(:,1), mu_map(:,2), sz, 'bs')
-% hold on
-% plot(mu_t(1), mu_t(2), 'r+', 'MarkerSize', sz-20, 'LineWidth', 2)
-% legend({'ML','MAP', 'true'},'FontSize',12)
-% xlabel('x_1')
-% ylabel('x_2')
+scatter(mu_ml(:,1), mu_ml(:,2), sz, 'gs')
+hold on
+scatter(mu_map(:,1), mu_map(:,2), sz, 'bs')
+hold on
+plot(mu_t(1), mu_t(2), 'r+', 'MarkerSize', sz-20, 'LineWidth', 2)
+legend({'ML','MAP', 'true'},'FontSize',12)
+xlabel('mu_1')
+ylabel('mu_2')
 
 m1 = mu_t(1);
 m2 = mu_t(2);
 lw = 2
-figure(1)
+figure(2)
 x = 1:1:n;
 plot(x, mu_ml(:,1), 'b', 'LineWidth', lw)
 hold on
