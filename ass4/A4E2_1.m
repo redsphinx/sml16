28c6d1359579c6e031b84cde30bfbb7e85ad5dad
redsphinx <gabi.ras@hotmail.com>
2017-01-10 15:23:52 +0100

    fixing epoch graphs to subplots

--- ass4/A4E2_1.m
+++ ass4/A4E2_1.m
@@ -1,7 +1,7 @@
 % clear stuff
 clear
 clc
-%%
+rng(42)
 % sample plot distribution
 sigma_square = eye(2) * 0.4;
 mu = [0 0];
@@ -10,5 +10,5 @@ x2 = x1;
 [X1, X2] = meshgrid(x1, x2);
 X = horzcat(X1(:), X2(:));
 Y = 3 * mvnpdf([X1(:) X2(:)], mu, sigma_square); 
-surf(X1, X2, reshape(Y, length(x1), length(x2)))
+% surf(X1, X2, reshape(Y, length(x1), length(x2)))
 
--- ass4/A4E2_2.m
+++ ass4/A4E2_2.m
@@ -1,13 +1,9 @@
-A4E2_1
-% implement neural net
-% input x = [x1, x2, bias]
-% output y
-% training data X, add bias don't forget
+% A4E2_2 
+A4E2_1 % execute script to make data X and Y
 
-% input nodes
-D = 2;
-% hidden nodes
-M = 8;
+% implement neural net
+D = 2; % input nodes
+M = 8; % hidden nodes
 % weights, with bias implied (in the +1)
 W1 = rand(D + 1, M) - 0.5;
 W2 = rand(M + 1, 1) - 0.5;
@@ -16,21 +12,26 @@ eta = 0.1;
 
 Yhat = zeros(length(X),1);
 for i = 1:length(X)
-    x_tmp = [X(i,:) 1];
-    z = [tanh(x_tmp * W1) 1];
+    x = [X(i,:) 1];
+    z = [tanh(x * W1) 1];
     y = z * W2;
     Yhat(i) = y;
 end
-figure;
+% figure;
 surf(X1, X2, reshape(Yhat, length(x1), length(x2)))
 title(sprintf('epoch 0'))
 %%
-for epoch = 1:200
+% A4E2_3
+number_of_epochs = 80;
+interval = 20;
+number_of_plots = number_of_epochs / interval;
+plot_counter = 0;
+for epoch = 1:number_of_epochs
     epoch
     Yhat = zeros(length(X),1);
     for i = 1:length(X)
-        x_tmp = [X(i,:) 1];
-        z = [tanh(x_tmp * W1) 1];
+        x = [X(i,:) 1];
+        z = [tanh(x * W1) 1];
         y = z * W2;
         Yhat(i) = y;
         % backpropagation
@@ -38,16 +39,22 @@ for epoch = 1:200
         delta_j = (1 - z.^2) .* W2' * delta_k; % 1x4 * 4x1 * 1x1 -> 1x4
         delta_j = delta_j(1:M);
         % (1 - z^2): da/dW1
-        dEdW1 = delta_j' * x_tmp;
+        dEdW1 = delta_j' * x;
         dEdW2 = delta_k * z';
 
         W1 = W1 - eta * dEdW1';
         W2 = W2 - eta * dEdW2;
     end
-    if(mod(epoch,20) == 0)
-        sprintf('plot')
-        figure;
+    if(mod(epoch,interval) == 0)
+        plot_counter = plot_counter + 1;
+%         sprintf('plot')
+%         figure;
+%         surf(X1, X2, reshape(Yhat, length(x1), length(x2)))
+%         title(sprintf('epoch: %d',epoch))
+
+        subplot(1,number_of_plots,plot_counter)
         surf(X1, X2, reshape(Yhat, length(x1), length(x2)))
+        axis square
         title(sprintf('epoch: %d',epoch))
     end
 end
