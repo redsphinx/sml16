823d2126127eba16a98e49a6b4b0864d93f42c89
J Ring <Jojanzing@192.168.2.3>
2017-01-11 23:58:04 +0100

    added plots of loglikelihood over multiple restarts to A4E3_1J

--- ass4/A4E3_1J.m
+++ ass4/A4E3_1J.m
@@ -4,57 +4,47 @@ clearvars -except X
 % number of clusters
 K = 4;
 N = length(X);
-
 dims = size(X,2);
-mus = repmat(mean(X),K,1) + (rand(K, dims)-0.5)*2;
-sigmas = repmat(eye(4)*(4*rand()+2),1,1,K);
 pis = zeros(1,K) + 1/K;
-
-respb = zeros(N,K);
 
 %% log likelihood + E Step
 cycles = 100;
-loglik = zeros(cycles,1);
-for i = 1:cycles
-    ln_pX = 0;
-    pxns = zeros(N,K);
-    for k = 1:K
-        pxn = pis(k)*mvnpdf(X,mus(k,:),sigmas(:,:,k));
-        pxns(:,k) = pxn;
+reps = 5;
+loglik = zeros(cycles,reps);
+
+for rep = 1:reps
+    mus = repmat(mean(X),K,1) + (rand(K, dims)-0.5)*2;
+    sigmas = repmat(eye(4)*(4*rand()+2),1,1,K);
+
+    respb = zeros(N,K);
+
+    for i = 1:cycles
+        ln_pX = 0;
+        pxns = zeros(N,K);
+        for k = 1:K
+            pxn = pis(k)*mvnpdf(X,mus(k,:),sigmas(:,:,k));
+            pxns(:,k) = pxn;
+        end
+        ln_pX = sum(log(sum(pxns,2)));
+        for k = 1:K
+            respb(:,k) = pxns(:,k)./sum(pxns,2);
+        end
+
+        loglik(i,rep) = ln_pX;
+    %% M Step
+        Nks = sum(respb);
+        for k = 1:K
+    %         mus(k,:) = sum(bsxfun(@times,respb(:,k),X))/Nks(k);
+            mus(k,:) = (respb(:,k)'*X)/Nks(k);
+        end
+        for k = 1:K
+            varx = bsxfun(@minus,X,mus(k,:));
+            sigmaskn = bsxfun(@times, permute(varx', [1 3 2]), permute(varx', [3 1 2]));
+            sigmak = sum(permute(repmat(respb(:,k),1,4,4), [2 3 1]).*sigmaskn,3)/Nks(k);
+            sigmas(:,:,k) = sigmak;
+        end
+        pis = Nks/N;
     end
-    ln_pX = sum(log(sum(pxns,2)));
-    for k = 1:K
-        respb(:,k) = pxns(:,k)./sum(pxns,2);
-    end
-        
-    
-%     for n = 1:N
-%         sumpxn = 0;
-%         pxns = zeros(1,K);
-%         for k = 1:K
-%             pxn = pis(k)*mvnpdf(X(n,:),mus(k,:),sigmas(:,:,k));
-%             sumpxn = sumpxn+pxn;
-%             pxns(k) = pxn;
-%         end
-%         for k = 1:K
-%             respb(n,k) = pxns(k)/sumpxn;
-%         end
-%         ln_pX = ln_pX + log(sumpxn);
-%     end
-    loglik(i) = ln_pX;
-%% M Step
-    Nks = sum(respb);
-    for k = 1:K
-%         mus(k,:) = sum(bsxfun(@times,respb(:,k),X))/Nks(k);
-        mus(k,:) = (respb(:,k)'*X)/Nks(k);
-    end
-    for k = 1:K
-        varx = bsxfun(@minus,X,mus(k,:));
-        sigmaskn = bsxfun(@times, permute(varx', [1 3 2]), permute(varx', [3 1 2]));
-        sigmak = sum(permute(repmat(respb(:,k),1,4,4), [2 3 1]).*sigmaskn,3)/Nks(k);
-        sigmas(:,:,k) = sigmak;
-    end
-    pis = Nks/N;
 end
 plot(1:length(loglik),loglik)
 
