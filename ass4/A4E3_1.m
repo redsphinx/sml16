855f07019ef968773c222978a93eba38f4209d67
redsphinx <gabi.ras@hotmail.com>
2017-01-11 16:50:32 +0100

    A4E3_2 made SIGMA a 3 dim matrix

--- ass4/A4E3_1.m
+++ ass4/A4E3_1.m
@@ -36,13 +36,13 @@ MU = zeros(K, 4);
 for i=1:K
     MU(i, :) = [mean(X(:,1)) + rand, mean(X(:,2)) + rand, mean(X(:,3)) + rand, mean(X(:,4)) + rand];
 end
-SIGMA = repmat(eye(4),1,K);
+SIGMA = repmat(eye(4),1,1,K);
 for j=1:K
     sigma_tmp = eye(4);
     for i=1:4
        sigma_tmp(i,:) = sigma_tmp(i,:).*(4*rand()+2);
     end
-    SIGMA(:, (j-1)*4+1:j*4) = sigma_tmp;
+    SIGMA(:, :, j) = sigma_tmp;
 end
 iterations = 100;
 
@@ -64,11 +64,11 @@ total_log_likelihood
 GAMMA = zeros(N, K); % responsibilities
 for i=1:N
     for j=1:K
-        p_x_i = PI(j)*mvnpdf(X(i,:), MU(j), SIGMA(:, (j-1)*4+1:j*4));% top in 9.23
+        p_x_i = PI(j)*mvnpdf(X(i,:), MU(j), SIGMA(:, :, j));% top in 9.23
         total_p_x = 0;
         % bot in 9.23
         for l=1:K
-            p_x = PI(l)*mvnpdf(X(i,:), MU(l), SIGMA(:, (l-1)*4+1:l*4));
+            p_x = PI(l)*mvnpdf(X(i,:), MU(l), SIGMA(:, :, l));
             total_p_x = total_p_x + p_x;
         end
         GAMMA(i,j) = p_x_i / total_p_x;
@@ -86,7 +86,7 @@ for j=1:K
     for i=1:N
        sigma_new_all = sum(GAMMA(i,j).*( (X(i,:)-MU(j)) * (X(i,:)-MU(j))' ) );
     end
-    SIGMA(:, (j-1)*4+1:j*4) = 1/N_k * sigma_new_all;
+    SIGMA(:, :, j) = 1/N_k * sigma_new_all;
     PI(j) = N_k/N;
 end
 %%
@@ -95,7 +95,7 @@ total_log_likelihood = 0;
 for i=1:N
     likelihood = 0;
     for j=1:K
-        likelihood_i = PI(j) * mvnpdf(X(i,:), MU(j,:), SIGMA(:, (j-1)*4+1:j*4));
+        likelihood_i = PI(j) * mvnpdf(X(i,:), MU(j,:), SIGMA(:, :, j));
         likelihood = likelihood + likelihood_i;
     end
     total_log_likelihood = total_log_likelihood + log(likelihood);
